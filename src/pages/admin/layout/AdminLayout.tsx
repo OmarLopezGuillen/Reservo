fb26f9e1c0c77911dff49d0b195d98cccd9504f1
AridaneRod <aridanerodriguezmoreno@gmail.com>
2025-11-09 15:28:22 +0000

    feat(admin-layout): añadir uso de useLocation para reiniciar Suspense en cambios de subruta

--- src/pages/admin/layout/AdminLayout.tsx
+++ src/pages/admin/layout/AdminLayout.tsx
@@ -1,16 +1,18 @@
 import { Suspense } from "react"
-import { NavLink, Outlet } from "react-router"
+import { NavLink, Outlet, useLocation } from "react-router"
 import { Loading } from "@/components/Loading"
 import { ROUTES } from "@/ROUTES"
 
 const AdminLayout = () => {
+	const location = useLocation()
 	return (
 		<div>
 			<NavLink to={ROUTES.ADMIN.AJUSTES}> Ajustes </NavLink>
 			<NavLink to={ROUTES.ADMIN.LIGA}> Liga </NavLink>
 			<NavLink to={ROUTES.ADMIN.ESTADISTICAS}> Estadisticas </NavLink>
 
-			<Suspense fallback={<Loading />}>
+			{/* Suspense que se reinicia cada vez que cambia la subruta */}
+			<Suspense key={location.pathname} fallback={<Loading />}>
 				<Outlet />
 			</Suspense>
 		</div>
